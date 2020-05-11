ed0591b38d695c403644ab9c483a8aa68b5207ef
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-05-10 23:03:29 -0400

    Done.

---
+++ build/precache-manifest.03657a94d2b845be08e18a8b48ca8abc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70529782e2ca96973849f434bcf78ba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927fbfa44be98a9afa1c",
+    "url": "/static/js/2.97b4407a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.97b4407a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c14a1c1091509975b13",
+    "url": "/static/js/main.2179d773.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
